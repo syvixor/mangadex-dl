fe847b27af0ba3540f7526379d9d0bc44112a2ee
syvixor <syvixor@proton.me>
2026-01-26 20:30:57 +0100

    chore: update version

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@
 command-line tool to download single or multiple manga chapters from mangadex with ease.
 """
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 __description__ = "command-line tool to download single or multiple manga chapters from mangadex with ease."
 __author__ = "syvixor"
 __author_email__ = "syvixor@proton.me"
